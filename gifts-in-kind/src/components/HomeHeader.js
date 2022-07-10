61641924ebaec2ad2d9a195be9526e49a2752457
Joanna <joanna.szczerbowska@gmail.com>
2022-07-10 18:20:21 +0200

    Header, statistics styles

--- gifts-in-kind/src/components/HomeHeader.js
+++ gifts-in-kind/src/components/HomeHeader.js
@@ -2,9 +2,13 @@
 const HomeHeader = () => {
     return (
         <div className="container header">
-            <img className="img" src="./assets/HeroImage.png" alt="different things" />
+            {/* <img className="img" src="./assets/HeroImage.png" alt="different things" /> */}
+            <div className="img"/>
             <div className="header__content">
-                <div><span>Zaloguj</span><span>Załóz konto</span></div>
+                <ul className="logIn__list">
+                    <li className="logIn__list--item">Zaloguj</li>
+                    <li className="logIn__list--item">Załóż konto</li>
+                </ul>
                 <nav className="header__nav ">
                     <ul className="nav__list">
                         <li className="nav__list--item">Start</li>
@@ -14,7 +18,7 @@ const HomeHeader = () => {
                         <li className="nav__list--item">Kontakt</li>
                     </ul>
                 </nav>
-                <span className="slogan">Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</span>
+                <span className="slogan">Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce<br/></span>
                 <div className="buttons__container">
                     <button className='btn'>Oddaj <br/>rzeczy</button>
                     <button className='btn'>Zorganizuj <br/>zbiórkę</button>
--- gifts-in-kind/src/components/Statistics.js
+++ gifts-in-kind/src/components/Statistics.js
@@ -1,6 +1,24 @@
 const Statistics = () => {
     return (
-        <p>Statistics</p>
+        <div className="stats">
+            <div className="stats__item">
+                <span className='stats__number'>10</span>
+                <span className='stats__title'>ODDANYCH WORKÓW</span>
+                <p className='stats__text'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div className="stats__item">
+                <span className='stats__number'>5</span>
+                <span className='stats__title'>WSPARTYCH ORGANIZACJI</span>
+                <p className='stats__text'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div className="stats__item">
+                <span className='stats__number'>7</span>
+                <span className='stats__title'>ZORGANIZOWANY ZBIÓREK</span>
+                <p className='stats__text'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+
+            </div>
+        </div>
+
     )
 }
 
