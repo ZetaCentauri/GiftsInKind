2343d17822b0594845214ead9d02a35c593cf133
Joanna <joanna.szczerbowska@gmail.com>
2022-07-11 20:13:29 +0200

    AboutUs Styles

--- gifts-in-kind/src/components/AboutUs.js
+++ gifts-in-kind/src/components/AboutUs.js
@@ -1,6 +1,12 @@
 const AboutUs = () => {
     return (
-        <p>AboutUs</p>
+        <div className="about_us">
+            <div className="about_us__content">
+                <span className="slogan">O nas</span>
+                <p className="about_us__text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+            </div>
+            <div className="about_us__img"/>
+        </div>
     )
 }
 
