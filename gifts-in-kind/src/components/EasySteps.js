bb54e995019862e8bb42b1fa6385e92313f326d5
Joanna <joanna.szczerbowska@gmail.com>
2022-07-11 19:49:44 +0200

    Easy steps styles

--- gifts-in-kind/src/components/EasySteps.js
+++ gifts-in-kind/src/components/EasySteps.js
@@ -1,6 +1,27 @@
 const EasySteps = () => {
     return (
-        <p>Easy steps</p>
+        <div className="easySteps">
+            <span className="slogan">Wystarczą 4 proste kroki<br/></span>
+            <div className="steps">
+                <div className="step">
+                    <span className="step__title">Wybierz rzeczy</span>
+                    <p className="step__text">ubrania, zabawki, sprzęt i inne</p>
+                </div>
+                <div className="step">
+                    <span className="step__title">Spakuj je</span>
+                    <p className="step__text">skorzystaj z worków na śmieci</p>
+                </div> 
+                <div className="step">
+                    <span className="step__title">Zdecyduj komu chcesz pomóc</span>
+                    <p className="step__text">wybierz zaufane miejsce</p>
+                </div>
+                <div className="step">
+                    <span className="step__title">Zamów kuriera</span>
+                    <p className="step__text">kurier przyjedzie w dogodnym terminie</p>
+                </div>
+            </div>
+            <button className="btn">Oddaj rzeczy</button>
+        </div>
     )
 }
 
